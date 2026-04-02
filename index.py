3e02ac033295fd4e31784e6efdc9560534d6d286
Dev-Ruqoyah <hamzatrukayatabiodun02@gmail.com>
2026-04-02 15:21:36 +0100

    Added the division function to the calculator function

--- index.py
+++ index.py
@@ -1,11 +1,13 @@
 print("Welcome to my calculator")
 def calculator():
     print("What do you want to do?: ")
-    menu = input("1.Addition 2.Subtraction ")
+    menu = input("1.Addition 2.Subtraction 3.Division ")
     if menu == "1":
         addition()
     elif menu == "2":
         subtraction()
+    elif menu == "3":
+        division()
     else:
         print("invalid input")
         
@@ -21,6 +23,14 @@ def subtraction():
     second = float(input("Enter the second number: "))
     result = first - second
     print(result)
+    
+def division():
+    first = float(input("Enter the first number: "))
+    second = float(input("Enter the second number: "))
+    result = first / second
+    print(result)
+    
+    
 calculator()
 
 
